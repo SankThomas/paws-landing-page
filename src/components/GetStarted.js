82f8238d1d3f73f351bd0825890d12d521559926
Thomas Sankara <thomassankara74@gmail.com>
2021-06-10 15:24:30 +0300

    Get Started component

---
+++ src/components/GetStarted.js
@@ -0,0 +1,11 @@
+const GetStarted = () => {
+  return (
+    <>
+      <section className="get-started">
+        <button className="btn-white">Get Started</button>
+      </section>
+    </>
+  )
+}
+
+export default GetStarted
