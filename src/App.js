b41f5b4d104246183606be9f0556dfa36183b8e3
Thomas Sankara <thomassankara74@gmail.com>
2021-06-10 13:15:26 +0300

    set up components, pages and react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom"
 
-function App() {
+// Components
+import Showcase from "./components/Showcase"
+import Services from "./components/Services"
+import Testimonials from "./components/Testimonials"
+import GetStarted from "./components/GetStarted"
+import Footer from "./components/Footer"
+
+// Pages
+import About from "./pages/About"
+import ContactUs from "./pages/ContactUs"
+import Error from "./pages/Error"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Showcase />
+          <Services />
+          <Testimonials />
+          <GetStarted />
+          <Footer />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact-us">
+          <ContactUs />
+        </Route>
+        <Route path="*">
+          <Error />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
