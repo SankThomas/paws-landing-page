6c70a9296373cfb2de9358453aa3ff9630ce41f3
Thomas Sankara <thomassankara74@gmail.com>
2021-06-10 15:55:37 +0300

    Testimonials component

---
+++ src/components/Testimonials.js
@@ -0,0 +1,27 @@
+import cathleen from "../images/cathleen.jpg"
+
+const Testimonials = () => {
+  return (
+    <>
+      <section className="testimonials">
+        <div>
+          <p>
+            Procuring as in resembled by in agreeable. Next long no gave mr
+            eyes. Admiration advantages no he celebrated so pianoforte
+            unreserved. Not its herself forming charmed amiable. Him why feebly
+            expect future now.{" "}
+          </p>
+          <h3>
+            Cathleen Alisa<span>, Paws Founder</span>
+          </h3>
+        </div>
+
+        <div>
+          <img src={cathleen} alt="Cathleen Alisa" />
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Testimonials
