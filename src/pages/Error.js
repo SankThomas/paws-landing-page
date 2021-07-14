b9a53d9985feb371d4a5a527f19f07a469836af1
Thomas Sankara <thomassankara74@gmail.com>
2021-07-14 09:30:48 +0300

    Error page

---
+++ src/pages/Error.js
@@ -0,0 +1,12 @@
+import { NavLink } from "react-router-dom"
+
+const Error = () => {
+  return (
+    <div>
+      <h1>Error | Page Not Found</h1>
+      <NavLink to="/">Home</NavLink>
+    </div>
+  )
+}
+
+export default Error
